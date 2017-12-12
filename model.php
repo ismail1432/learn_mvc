dda8d36c76a2feddef67a7dd73a4a11ba2d18d05
Smaine Milianni <contact@smaine.me>
2017-12-12 14:23:51 +0100

    Update model

--- model.php
+++ model.php
@@ -11,6 +11,6 @@ function getBillets()
     }
 
     $req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
-
-    return $req;
+    $datas = $req->fetchAll();
+    return $datas;
 }
